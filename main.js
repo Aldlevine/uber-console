8a1b426c219cf785362c368bc2720001aa700d9c
Aaron Levine <ald.levine@gmail.com>
2016-02-12 17:02:51 -0500

    Setup initial project

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+app.on('window-all-closed', function(){
+  app.quit();
+});
+
+app.on('ready', function(){
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+});
